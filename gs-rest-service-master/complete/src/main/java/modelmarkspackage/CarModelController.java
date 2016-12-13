08c0fc660ef621aeb4fd6d05e95389814d6f6b0d
tabledott <tabledott@gmail.com>
2016-12-13 18:05:10 +0200

    almost ready.

--- gs-rest-service-master/complete/src/main/java/modelmarkspackage/CarModelController.java
+++ gs-rest-service-master/complete/src/main/java/modelmarkspackage/CarModelController.java
@@ -1,13 +1,21 @@
 package modelmarkspackage;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarModelController {
-    @RequestMapping("/listmodels")
-    public CarModel listModels() {
-    	return CarModelDao.listAllCarMake();    	
+    @RequestMapping("/listmakes")
+    public CarModel listMakes() {
+    	
+    	ApplicationContext context =
+        		new ClassPathXmlApplicationContext( "Spring-Module.xml" );
+
+    	 CarModelDao carModelDAO = (CarModelDao) context.getBean("carModelDao");
+
+    	return carModelDAO.listAllCarMake();    	
   }
 
 }
