ebd10e46c559fa58587434642bcddd3d7b560008
tabledott <tabledott@gmail.com>
2016-12-13 11:49:58 +0200

    test

--- gs-rest-service-master/complete/src/main/java/modelmarkspackage/Application.java
+++ gs-rest-service-master/complete/src/main/java/modelmarkspackage/Application.java
@@ -1,5 +1,7 @@
 package modelmarkspackage;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,16 +12,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
+        
     	ApplicationContext context =
-        		new ClassPathXmlApplicationContext("Spring-Module.xml");
-
-            CarModelDao carModelDAO = (CarModelDao) context.getBean("customerDAO");
+        		new ClassPathXmlApplicationContext( "Spring-Module.xml" );
+    	
+            CarModelMarksDao carModelDAO = (CarModelMarksDao) context.getBean("carModelMarksDao");
             //Customer customer = new Customer(1, "mkyong",28);
             //customerDAO.insert(customer);
 
-            Customer customer1 = customerDAO.findByCustomerId(1);
-            System.out.println(customer1);
-
+            CarModelMarks cars1 = carModelDAO.findCarModelsByMark("ford");
+            for(int i = 0; i < cars1.getModels().size(); i++){
+            	System.out.println(cars1.getModels().get(i));
+            }
 
         SpringApplication.run(Application.class, args);
     }
